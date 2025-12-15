f00acc61c7fc80bfd92ff5d0f659bd1896301cbb
ghsss <gabrielhsaldanha@gmail.com>
2025-12-15 15:55:02 -0300

    Implementando o método calcularTotalXp() sem usar Stream API.

---
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,39 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.Objects;
+
+public abstract class Conteudo {
+    protected static final double XP_PADRAO = 10d;
+    private String titulo;
+    private String descricao;
+
+    public abstract double calcularXp();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Conteudo conteudo = (Conteudo) o;
+        return Objects.equals(titulo, conteudo.titulo) && Objects.equals(descricao, conteudo.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, descricao);
+    }
+}
